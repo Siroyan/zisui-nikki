8f871eaa7d179407f86f42780707a99cb84b6c95
Siroyan <1517053@ed.tus.ac.jp>
2020-09-16 14:47:31 +0000

    [add] setting of deploying s3

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
-from aws_cdk import core
+from aws_cdk import (
+    core,
+    aws_s3 as s3,
+    aws_s3_deployment as s3_deploy
+)
+import os
 
 from zisui_nikki.zisui_nikki_stack import ZisuiNikkiStack
 
-
 app = core.App()
-ZisuiNikkiStack(app, "zisui-nikki")
+ZisuiNikkiStack(
+    app, "zisui-nikki",
+    env = {
+        "region": os.environ["CDK_DEFAULT_REGION"],
+        "account": os.environ["CDK_DEFAULT_ACCOUNT"]
+    }
+)
 
 app.synth()
--- zisui_nikki/zisui_nikki_stack.py
+++ zisui_nikki/zisui_nikki_stack.py
@@ -1,9 +1,26 @@
-from aws_cdk import core
-
+from aws_cdk import (
+    core,
+    aws_s3 as s3,
+    aws_s3_deployment as s3_deploy
+)
+import os
 
 class ZisuiNikkiStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        bucket = s3.Bucket(
+            self,"zisui-nikki.net",
+            bucket_name = "zisui-nikki.net",
+            website_index_document = "index.html",
+            public_read_access = True,
+            removal_policy = core.RemovalPolicy.DESTROY
+        )
+
+        s3_deploy.BucketDeployment(
+            self, "BucketDeployment",
+            destination_bucket = bucket,
+            sources = [s3_deploy.Source.asset("./gui/dist")],
+            retain_on_delete = False,
+        )
