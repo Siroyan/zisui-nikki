d543c86255ad9c189b40cfbadb2827450962217e
Siroyan <1517053@ed.tus.ac.jp>
2020-09-26 09:52:42 +0000

    [修正] filterのエラーを修正

--- gui/src/components/MealList.vue
+++ gui/src/components/MealList.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="pb-4">
     <!-- 投稿が存在する場合 -->
-    <div v-if="mealsOfSelectedDay.length">
-      <div v-for="meal in mealsOfSelectedDay" :key="meal.id" class="card mb-4">
-        <img src="../assets/dummy-meal.jpg" class="card-img-top">
-        <div class="card-body">
-          <h5 class="card-title">{{meal.date.getFullYear()}}年{{meal.date.getMonth()+1}}月{{meal.date.getDate()}}日</h5>
-          <p class="card-text">{{meal.title}}</p>
-          <p class="card-text">{{meal.cost}}円</p>
-        </div>
+    <div v-for="meal in mealsOfSelectedDay" :key="meal.id" class="card mb-4">
+      <img src="../assets/dummy-meal.jpg" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">{{meal.date.getFullYear()}}年{{meal.date.getMonth()+1}}月{{meal.date.getDate()}}日</h5>
+        <p class="card-text">{{meal.title}}</p>
+        <p class="card-text">{{meal.cost}}円</p>
       </div>
     </div>
-    <!-- 投稿が存在する場合 -->
-    <div v-else class="text-center">
+    <!-- 投稿が存在しない場合 -->
+    <div v-show="!mealsOfSelectedDay.length" class="text-center">
       投稿がありません
     </div>
   </div>
@@ -24,17 +22,17 @@ export default {
   props: {
     meals: {
       type: Array,
-      required: true
+      required: false
     },
     selectedDay: {
       type: Date,
-      required: true
+      required: false
     }
   },
   computed: {
     mealsOfSelectedDay: function() {
       const selectedDay = this.selectedDay;
-      return this.meals.filter(meal => {
+      return (this.meals || []).filter(meal => {
         return meal.date.getTime() === selectedDay.getTime();
       });
     },
