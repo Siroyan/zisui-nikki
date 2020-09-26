d5c33142ca98547d77ba0d5cdcc1808b2d64c499
Siroyan <1517053@ed.tus.ac.jp>
2020-09-26 07:40:43 +0000

    [削除] vue-js-modal削除

--- gui/src/main.js
+++ gui/src/main.js
@@ -3,10 +3,8 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VCalendar from 'v-calendar'
-import VModal from 'vue-js-modal'
 
 Vue.use(VCalendar)
-Vue.use(VModal)
 Vue.config.productionTip = false
 
 new Vue({
